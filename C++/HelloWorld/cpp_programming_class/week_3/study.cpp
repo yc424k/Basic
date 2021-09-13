db4049a69b955a26f4b23adfe0158b230f7ce221
You_Blue <yc424k@gmail.com>
2021-09-13 10:54:20 +0900

    Class Basic

--- C++/HelloWorld/cpp_programming_class/week_3/study.cpp
+++ C++/HelloWorld/cpp_programming_class/week_3/study.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 using namespace std;
 
-class Circle
+class Rectangle
 {
 public:
-    int radius;
-    double getArea();
+    unsigned int width;
+    unsigned int height;
+    unsigned int getArea();
 };
 
-double Circle::getArea(){
-    return 3.14*radius*radius;
+unsigned int Rectangle::getArea()
+{
+    return width * height;
 }
 
 int main()
 {
-    Circle donut;
-    donut.radius = 1;
-    double area = donut.getArea();
-    cout << "donut 면적은 " << area << endl;
-
-    Circle pizza;
-    pizza.radius = 30;
-    area = pizza.getArea();
-    cout << "pizza 면적은 " << area << endl;
+    Rectangle rect;
+    rect.width = 3;
+    rect.height = 5;
+    cout << "사각형의 면적은 " << rect.getArea() << endl;
     return 0;
 }
