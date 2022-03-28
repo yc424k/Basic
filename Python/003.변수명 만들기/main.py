12a770a95f36bded98a54ba7dfca07db2edba679
yc424k <yc424k@gmail.com>
2022-03-28 23:27:28 +0900

    2022년 3월 27일 파이썬 공부

---
+++ Python/003.변수명 만들기/main.py
@@ -0,0 +1,6 @@
+ # 변수명의 첫 문자는 '_' or 영문자
+_myname = 'samsjang'
+my_name = '홍길동'
+MyName2 = 'Hong gil-dong'
+counter1 = 1
+Counter2 = 2
---
+++ Python/004.변수에 값 대입하기/main.py
@@ -0,0 +1,6 @@
+number1 = 1             # 정수
+pi = 3.14               # 실수
+flag = True             # boolean
+char = 'x'              # 문자
+chars = 'I Love Python' # 문자열
+ # 변수의 자료형은 대입되는 값에 따라 유동적으로 정해짐
