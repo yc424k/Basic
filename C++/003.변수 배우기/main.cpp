20aa7c9f537ad8556656ff313dd084f49a225270
yc424k <yc424k@gmail.com>
2022-07-02 03:11:15 +0900

    변수

--- C++/003.변수 배우기/main.cpp
+++ C++/003.변수 배우기/main.cpp
@@ -1,3 +1,15 @@
 //
 // Created by Blue You on 2022/07/02.
 //
+#include <iostream>
+
+using namespace  std;
+
+int main(){
+    int one = 1;
+    int two = 2;
+    int sum = one + two;
+    cout << "1 + 2 = " << sum << endl;
+
+    return 0;
+}
