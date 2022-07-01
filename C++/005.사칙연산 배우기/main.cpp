e5fd4b0e26d6e6236665ffb50f114bc745b4f12e
yc424k <yc424k@gmail.com>
2022-07-02 03:16:48 +0900

    fixed untracked files

---
+++ C++/005.사칙연산 배우기/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
+
+    cout << one + three << endl;
+    cout << four - two << endl;
+    cout << two * three << endl;
+    cout << four / two << endl;
+
+    cout << two + three * four << endl;
+    cout << one + four / two << endl;
+
+    return 0;
+}
