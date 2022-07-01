9d8c276b7eae6a4e9ee35b9f8cd17b9ad1e55670
yc424k <yc424k@gmail.com>
2022-07-02 03:52:19 +0900

    #include

---
+++ C++/014.#include 배우기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> exam;
+    exam.push_back(10);
+    exam.push_back(20);
+    exam.push_back(30);
+
+    for (int i = 0, size = exam.size(); i < size; i++)
+    {
+        cout << "벡터 값 : " << exam.at(i) << endl;
+    }
+
+    return 0;
+}
