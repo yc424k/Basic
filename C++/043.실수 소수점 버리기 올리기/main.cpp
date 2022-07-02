861ede1c855d5505ea8f4a774df83fa4c9d33db5
yc424k <yc424k@gmail.com>
2022-07-02 18:41:24 +0900

    ceil, floor

---
+++ C++/043.실수 소수점 버리기 올리기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "== 소수점 버리기 ==" << endl;
+    cout << "==floor==" << endl;
+    cout << floor(1.1) << endl;
+    cout << floor(-2.3) << endl;
+
+    cout << "==소수점 올리기==" << endl;
+    cout << "==ceil==" << endl;
+    cout << ceil(1.1) << endl;
+    cout << ceil(-2.3) << endl;
+
+    return 0;
+}
