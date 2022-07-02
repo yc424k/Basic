4c1e5fc952519388014a48cf87ae628fa882f113
yc424k <yc424k@gmail.com>
2022-07-02 16:55:13 +0900

    ++,--

---
+++ C++/023.증감 연산자 이해하기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 1;
+    cout << x++ << endl;
+    cout << ++x << endl;
+    cout << x-- << endl;
+    cout << --x << endl;
+
+    return 0;
+}
