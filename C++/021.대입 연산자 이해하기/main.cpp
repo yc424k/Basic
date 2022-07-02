a183e9189e41f28edce8b8ecffd201b9ba7fd51d
yc424k <yc424k@gmail.com>
2022-07-02 16:49:50 +0900

    =

---
+++ C++/021.대입 연산자 이해하기/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 1;
+    int y = 9;
+
+    int z = x + y;
+
+    double i = 1.2;
+    double j = 2.3;
+
+    double k = i + j;
+
+    cout << z << endl;
+    cout << k << endl;
+
+    return 0;
+}
