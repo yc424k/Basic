10ae4b605df499550f1165c6ded45de1c768110d
yc424k <yc424k@gmail.com>
2022-07-02 17:21:52 +0900

    if~else if~else

---
+++ C++/032.중첩 조건문 이해하기/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1 = 100;
+    int number2 = 6;
+
+    if (number1 < number2)
+        cout << "number2가 더 큽니다." << endl;
+    else if(number1 > number2)
+        cout << "number1이 더 큽니다." << endl;
+    else
+        cout << "number1과 number2는 같습니다." << endl;
+
+    if (number2 < 7)
+    {
+        if (number2 > 3)
+        {
+            if (number2 == 4){
+                
+            }
+                
+        }
+    }
+}
