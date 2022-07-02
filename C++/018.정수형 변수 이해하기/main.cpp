a834e359659062dae5e8ed4ac032a8ef0dd4beb8
yc424k <yc424k@gmail.com>
2022-07-02 16:43:47 +0900

    int

---
+++ C++/018.정수형 변수 이해하기/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int positive = 100;
+    int negative = -200;
+    int ascii_value = 'A';
+
+    cout << positive << endl;
+    cout << negative << endl;
+    cout << ascii_value << endl;
+
+    return 0;
+}
