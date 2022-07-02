5d3fbecf84c7a32facfc16a28a23da833da8e36c
yc424k <yc424k@gmail.com>
2022-07-02 16:53:42 +0900

    +, -

---
+++ C++/022.부호 연산자 이해하기/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 1;
+    int y = -1;
+    int z = -2;
+
+    cout << x+ y << endl;
+    cout << y + z << endl;
+
+    return 0;
+}
