9565daeb7952386d09ffa7f88bc8bbc1169a2869
yc424k <yc424k@gmail.com>
2022-07-03 16:48:39 +0900

    localtime

---
+++ C++/051.날짜와 시간을 문자열로 변환하기/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    time_t now = time(NULL);
+    tm* ptm = localtime(&now);
+
+    char buffer[64];
+    strftime(buffer, 64, "예제 만드는 지금은 %Y %m월 %d일, %H시 %M분 %S초 입니다.(%p)\n ", ptm);
+
+    cout << buffer;
+
+    return 0;
+}
