16d55c8d70b4b44944f0a184fa4cbd101330656a
yc424k <yc424k@gmail.com>
2022-07-04 01:07:57 +0900

    current_path

---
+++ C++/097.프로그램 실행 폴더 알아내기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <experimental/filesystem>
+
+using namespace std;
+
+namespace fs = experimental::filesystem;
+
+int main()
+{
+    cout << "프로젝트 폴더 : " << fs::current_path() << endl;
+
+    return 0;
+}
