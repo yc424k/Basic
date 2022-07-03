a9be1cee08da145eecd53434e889a343b565e3b5
yc424k <yc424k@gmail.com>
2022-07-03 20:00:40 +0900

    erase, rmove

---
+++ C++/061.문자열에서 특정 문자만 제거하기/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string sentence = "i like coding";
+
+    sentence.erase(remove(sentence.begin(), sentence.end(), ' '), sentence.end());
+
+    cout << sentence << endl;
+
+    return 0;
+}
