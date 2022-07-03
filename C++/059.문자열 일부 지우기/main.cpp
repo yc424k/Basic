18bb6e05e96dfb75f4636d4e115086192340a28a
yc424k <yc424k@gmail.com>
2022-07-03 18:55:35 +0900

    erase

---
+++ C++/059.문자열 일부 지우기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string sentence = "i hate coding";
+    sentence.erase(0, 7);
+    cout << "i like " << sentence << endl;
+
+    return 0;
+}
