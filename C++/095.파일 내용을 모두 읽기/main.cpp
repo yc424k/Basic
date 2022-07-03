7f2e1a55f737d8276cab01deb51bf09ecd3fbac3
yc424k <yc424k@gmail.com>
2022-07-04 01:00:21 +0900

    ifstream

---
+++ C++/095.파일 내용을 모두 읽기/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream ifs("095.txt");
+
+    stringstream ss;
+
+    ss << ifs.rdbuf();
+    ifs.close();
+
+    string read = "";
+
+    cout << "== !ss.eof ==" <<endl;
+    while(!ss.eof())
+    {
+        ss >> read;
+        cout << read << " ";
+    }
+    cout << endl << "== !ss.str() ==" << endl;
+    read = ss.str();
+    cout << endl << read << endl;
+
+    return 0;
+}
