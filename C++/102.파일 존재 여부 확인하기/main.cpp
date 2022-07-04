add82420135375d48a7f88fed1ab457795ad1517
yc424k <yc424k@gmail.com>
2022-07-04 16:27:33 +0900

    good

---
+++ C++/102.파일 존재 여부 확인하기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream stream;
+    stream.open("C:\\BOOOTNXT");
+
+    if (stream.good() == true)
+        cout << "파일이 존재합니다." << endl;
+    else
+        cout << "파일이 없습니다." << endl;
+
+    stream.close();
+
+    return 0;
+}
