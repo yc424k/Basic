463031719fe40ec00ea1f71c398b17bf82b48fb4
yc424k <yc424k@gmail.com>
2022-07-04 22:23:41 +0900

    friend 함수는 private 변수에도 접근이 가능

---
+++ C++/119.Friend 함수 사용하기/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ondal
+{
+public:
+    friend void GetYear(Ondal ondal)
+    {
+        cout << "아단선 전투 연도" << ondal.year_ << endl;
+    }
+    void SetYear(int year) { year_ = year; }
+
+private:
+    int year_;
+};
+
+int main()
+{
+    Ondal ondal;
+    ondal.SetYear(590);
+
+    GetYear(ondal);
+    return 0;
+}
