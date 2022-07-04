650607b4db9afefa3e4c551820dffb707f9fc52d
yc424k <yc424k@gmail.com>
2022-07-04 21:50:31 +0900

    private

---
+++ C++/112.객체지향 정보은닉 이해하기/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class KingInfo
+{
+public:
+    KingInfo(){};
+
+public:
+    void SetValue(const string value)
+    {
+        if (value.empty() == false)
+            value_ = value;
+        else
+            cout << "잘못된 인자!" << endl;
+    }
+    string GetValue() const
+    {
+        if (value_.empty() == false)
+            return value_;
+        else
+            return "설정을 먼저 해주세요.";
+    }
+
+private:
+    string value_;
+    int number_;
+};
+
+int main()
+{
+    KingInfo king_info1;
+    king_info1.SetValue("조선 중종 이역");
+
+    KingInfo king_info2;
+    king_info2.SetValue("조선 인종 이호");
+
+    cout << king_info1.GetValue() << endl;
+    cout << king_info2.GetValue() << endl;
+
+    return 0;
+}
