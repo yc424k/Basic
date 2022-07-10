43c652f29116b2d5fda5c37d713905a7c1181186
yc424k <yc424k@gmail.com>
2022-07-10 14:36:05 +0900

    lambda

---
+++ Python/135.이름없는 한줄짜리 함수 만들기/main.py
@@ -0,0 +1,17 @@
+def add(x, y): return x + y
+
+
+ret = add(1, 3)
+print(ret)
+
+funcs = [lambda x:x+'pptx', lambda x: x+'.docx']
+ret1 = funcs[0]('intro')
+ret2 = funcs[1]('Report')
+
+print(ret1)
+print(ret2)
+
+names = {'Marry': 10999, 'Sams': 2111, 'Aimy': 9778,
+         'Tom': 20245, 'Michale': 27115, 'Bob': 5887, 'Kelly': 7855}
+ret3 = sorted(names.items(), key=lambda x: x[0])
+print(ret3)
