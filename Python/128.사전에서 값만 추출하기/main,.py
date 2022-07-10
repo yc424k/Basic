23ca9edff3563ab142941c063648a6df7e3724e7
yc424k <yc424k@gmail.com>
2022-07-10 14:09:39 +0900

    values

---
+++ Python/128.사전에서 값만 추출하기/main,.py
@@ -0,0 +1,10 @@
+names = {'Marry': 10999, 'Sams': 2111, 'Aimy': 9778,
+         'Tom': 20245, 'Michale': 27115, 'Bob': 5887, 'Kelly': 7855}
+vals = names.values()
+print(vals)
+
+vals_list = list(vals)
+print(vals_list)
+
+ret = sum(vals_list)
+print('출생아 수 총계:%d' % (ret))
