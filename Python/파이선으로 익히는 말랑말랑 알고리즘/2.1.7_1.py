e0b586361c641ab18580886f56b48f5f558e9925
yc424k <yc424k@gmail.com>
2022-07-10 23:13:50 +0900

    .append()로 값 초기화하기

--- Python/파이선으로 익히는 말랑말랑 알고리즘/2.1.7_1.py
+++ Python/파이선으로 익히는 말랑말랑 알고리즘/2.1.7_1.py
@@ -7,3 +7,6 @@ for idx in range(len(string)):
 print(array, len(array))
 print("index 0 :", array[0])
 print("index 1 :", array[1])
+
+array1 = [0 for idx in range(len(string))]  # 2~5줄을 1줄로 압축시킴
+print(array)
