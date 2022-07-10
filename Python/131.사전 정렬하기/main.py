837cb1262fb2909b3d2e56ac2bf3875b2213a05c
yc424k <yc424k@gmail.com>
2022-07-10 14:22:58 +0900

    lambda

---
+++ Python/131.사전 정렬하기/main.py
@@ -0,0 +1,23 @@
+names = {'Marry': 10999, 'Sams': 2111, 'Aimy': 9778,
+         'Tom': 20245, 'Michale': 27115, 'Bob': 5887, 'Kelly': 7855}
+
+ret1 = sorted(names)
+print(ret1)
+
+
+def f1(x):
+    return x[0]
+
+
+def f2(x):
+    return x[1]
+
+
+ret2 = sorted(names.items(), key=lambda x: x[0])
+print(ret2)
+
+ret3 = sorted(names.items(), key=lambda x: x[1])
+print(ret3)
+
+ret4 = sorted(names.items(), key=lambda x: x[1], reverse=True)
+print(ret4)
