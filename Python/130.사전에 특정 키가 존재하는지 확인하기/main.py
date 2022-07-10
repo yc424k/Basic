4fc2d86dd769ac4bbccfd3f170c91283bc4acc6f
yc424k <yc424k@gmail.com>
2022-07-10 14:13:19 +0900

    in

---
+++ Python/130.사전에 특정 키가 존재하는지 확인하기/main.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+
+
+names = {'Marry': 10999, 'Sams': 2111, 'Aimy': 9778,
+         'Tom': 20245, 'Michale': 27115, 'Bob': 5887, 'Kelly': 7855}
+
+k = input('이름을 입력하세요:')
+if k in names:
+    print('이름이<%s>인 출생아 수는 <%d>명입니다.' % (k, names[k]))
+else:
+    print('자료에<%s>인 이름이 존재하지 않습니다.' % k)
