05b6795798106f6a6926fa019dc6055c18ab70f2
yc424k <yc424k@gmail.com>
2022-07-10 19:02:41 +0900

    os.remove

---
+++ Python/147.파일 삭제하기/main.py
@@ -0,0 +1,10 @@
+from os import remove
+from socketserver import ThreadingUnixDatagramServer
+
+target_file = '/home/yc424k/Basic/Python/stockcode.txt'
+
+k = input('[%s]파일을 삭제하겠습니까? (y/n) : ' % target_file)
+
+if k == 'y':
+    remove(target_file)
+    print('[%s]를 삭제했습니다.' % target_file)
